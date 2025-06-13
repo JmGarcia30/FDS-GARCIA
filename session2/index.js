42b79fedfc81c74fa6e1922a27b47db0c6c047c2
JmGarcia30 <jmjgarcia.student@ua.edu.ph>
2025-06-13 08:20:36 +0800

    Initital Commit

---
+++ session2/index.js
@@ -0,0 +1,61 @@
+// console.log("Hello, World!");
+
+//JS Basic Function
+
+function sayHello(){
+    console.log("Hello!");
+}
+
+// Function caling // Invocation
+
+sayHello();
+sayHello();
+sayHello();
+sayHello();
+
+function completeName(){
+    // Local Scoped -> full name. It is inside the function
+    let fullName = "Juan Dela Cruz";
+    console.log(fullName);
+}
+
+completeName();
+// console.log(fullName);
+
+// Global and Local Scope Variables
+
+// Global Scope
+let age = 20;
+
+{
+    // local scoped
+    const pi = 3.14
+    console.log(age);
+}
+
+// Accessing the variable inside the curly braces
+// console.log(pi); -> error not defined
+
+let studentName = "Marry Poppins";
+
+function getName(){
+    let age = 50;
+    console.log(studentName + "age is " + age);
+}
+
+getName();
+
+// Function with Alert
+
+function helloAlert(){
+    alert("Hello, World!");
+}
+
+// IT is not necessary to call a function in the js file. We can also call a function in the Console Panel
+
+// Function with prompt()
+
+function enterName(){
+    let inputName = prompt("Please Enter Your Name");
+    console.log("Hello there, " + inputName + "!")
+}
